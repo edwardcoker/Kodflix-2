32e1eb4ac2948bb5439224032ae398bb037de2c7
Edward Coker <edwardcoker@Edwards-MBP.home>
2019-08-11 11:30:03 +0100

    updated chnages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Edward, It's great to have you at Kodflix!!!!
+          Welcome Edward, It's great to have you at Kodflix.Happy Sunday!!!!
         </p>
         <a
           className="App-link"
