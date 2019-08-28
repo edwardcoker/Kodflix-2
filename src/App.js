53bc070e8029b55ea7f9638e201d0625df175aae
Edwward Coker <edwardcoker27@gmail.com>
2019-08-28 17:02:11 +0100

    Updated the app.js file

--- src/App.js
+++ src/App.js
@@ -1,62 +1,22 @@
 import React from 'react';
-import escape from './images/escape.jpg';
-import exposed from './images/exposed.jpg';
-import lastKingdom from './images/last-kingdom.jpg';
-import newMoon from './images/new-moon.jpg';
-import oath from './images/oath.jpg';
-import rampage from './images/rampage.jpg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Gallery from './Gallery';
+import Details from './Details/Details';
+import NotFound from './NotFound';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className='container'>
-        <div className='item'>
-          <img src={escape} alt='escape movie cover' />
-          <div className='overlay'>
-            <h1>Escape</h1>
-          </div>
-        </div>
-        <div className='item'>
-          <img src={exposed} alt='exposed movie cover' />
-          <div className='overlay'>
-            <h1>Exposed</h1>
-          </div>
-          </div>
-        <div className='item'>
-          <img src={lastKingdom} alt='last kingdom movie cover' />
-          <div className='overlay'>
-            <h1>Last kingdom</h1>
-          </div>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='item'>
-          <img src={newMoon} alt='new moon movie cover' />
-          <div className='overlay'>
-            <h1>New Moon</h1>
-          </div>
-          </div>
-        <div className='item'>
-          <img src={oath} alt='oath movie cover' />
-          <div className='overlay'>
-            <h1>Oath</h1>
-          </div>
-          </div>
-        <div className='item'>
-          <img src={rampage} alt='rampage movie cover' />
-          <div className='overlay'>
-            <h1>Rampage</h1>
-          </div>
-          </div>
-
-      </div>
-    </div>
-
-
-
-  );
+    return (
+        <Router>
+            <div className='App'>
+                <Switch>
+                    <Route exact path='/' component={Gallery} />
+                    <Route exact path='/not-found'component={NotFound} />
+                    <Route exact path='/:galleryId' component={Details} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
